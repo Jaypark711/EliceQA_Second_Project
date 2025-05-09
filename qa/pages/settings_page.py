bf2a7540a84e9a9ae9c83cab4dfc52996cbdec79
Jaypark711 <lossers0@gmail.com>
2025-05-09 17:03:47 +0900

    [ADD] logout 버튼 action 추가

--- qa/pages/settings_page.py
+++ qa/pages/settings_page.py
@@ -1,7 +1,15 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
 class SettingsPage(BasePage):
     # 로케이터 정의
+    URL_LINK_INPUT = (By.CSS_SELECTOR,'input[placeholder="URL of profile picture"]')
+    LOGOUT_BTN = (By.XPATH, '//button[text()="Or click here to logout."]')
+
+
 
     def __init__(self, driver):
         super().__init__(driver)
+
+    def click_logout_btn(self):
+        self.click_element(self.LOGOUT_BTN)
