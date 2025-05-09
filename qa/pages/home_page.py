d105bfa3305d530b62cf21131f165bc3f2adc53a
kys0529 <rkddustn2519@naver.com>
2025-05-09 16:35:46 +0900

    [ADD] 요소 및 함수 추가

--- qa/pages/home_page.py
+++ qa/pages/home_page.py
@@ -6,9 +6,9 @@ from pages.base_page import BasePage
 class HomePage(BasePage):
     # 로케이터 정의
     HOME_LINK = (By.XPATH, '//a[text()="Home"]')
-    SIGN_IN_LINK = (By.XPATH, '//a[@href="/login"]')
-    SIGN_UP_LINK = (By.XPATH, '//a[@href="/register"]')
-    USER_PROFILE_LINK = (By.XPATH, f"//a[@href='/{VALID_USER["username"]}']")
+    SIGN_IN_LINK = (By.CSS_SELECTOR, 'a[href="/login"]')
+    SIGN_UP_LINK = (By.CSS_SELECTOR, 'a[href="/register"]')
+    USER_PROFILE_LINK = (By.CSS_SELECTOR, f'a[href="/@{VALID_USER["username"]}"]')
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -21,3 +21,9 @@ class HomePage(BasePage):
 
     def click_sign_up_link(self):
         self.click_element(self.SIGN_UP_LINK)
+
+    def wait_for_user_profile_link(self):
+        self.find_element(self.USER_PROFILE_LINK)
+
+    def get_username_link_text(self):
+        href = self.get_attribute(self.USER_PROFILE_LINK, "href")
