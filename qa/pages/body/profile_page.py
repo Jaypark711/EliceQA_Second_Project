5de3ecdd81f1e8cf6b0d97a61bb1804c5e2c8404
kys0529 <rkddustn2519@naver.com>
2025-05-10 18:22:11 +0900

    [ADD] 프로필 페이지 요소 및 함수 추가

--- qa/pages/body/profile_page.py
+++ qa/pages/body/profile_page.py
@@ -1,7 +1,24 @@
+from selenium.webdriver.common.by import By
+
 from pages.base_page import BasePage
 
 class ProfilePage(BasePage):
     # 로케이터 정의
+    PROFILE_IMG = (By.CLASS_NAME, 'user-img')
+    USERNAME_TITLE = (By.TAG_NAME, 'h4')
+    BIO_P = (By.CSS_SELECTOR, 'h4 + p')
+    EDIT_PROFILE_SETTINGS_BTN = (By.CSS_SELECTOR, 'a[href="/settings"]')
+    MY_ARTICLE_TAB_LINK = (By.XPATH, '//a[text() = "My Articles"]')
+    MY_FAVORITED_TAB_LINK = (By.XPATH, '//a[text() = "Favorited Articles"]')
 
     def __init__(self, driver):
         super().__init__(driver)
+
+    def click_edit_profile_settings_btn(self):
+        self.click_element(self.EDIT_PROFILE_SETTINGS_BTN)
+
+    def click_article_tab_link(self):
+        self.click_element(self.MY_ARTICLE_TAB_LINK)
+
+    def click_my_favorited_tab_link(self):
+        self.click_element(self.MY_FAVORITED_TAB_LINK)
