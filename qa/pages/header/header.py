254943377bba633c78bb9297f26c5146b163abb0
kys0529 <rkddustn2519@naver.com>
2025-05-10 18:55:42 +0900

    [UPDATE] header 변경된 변수명에 맞춰 수정

--- qa/pages/header/header.py
+++ qa/pages/header/header.py
@@ -38,10 +38,10 @@ class Header(BasePage):
         self.click_element(self.MY_PROFILE_LINK)
 
     def wait_for_user_profile_link_appear(self):
-        self.find_element(self.USER_PROFILE_LINK)
+        self.find_element(self.MY_PROFILE_LINK)
 
     def is_user_profile_link_disappear(self):
-        return self.is_element_disappear(self.USER_PROFILE_LINK)
+        return self.is_element_disappear(self.MY_PROFILE_LINK)
 
     def get_username_link_text(self):
-        return self.get_attribute(self.USER_PROFILE_LINK, "href")
+        return self.get_attribute(self.MY_PROFILE_LINK, "href")
