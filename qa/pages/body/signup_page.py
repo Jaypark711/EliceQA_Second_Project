60c643bc8b018400ec77e2251854da26dd8ce56b
kys0529 <rkddustn2519@naver.com>
2025-05-11 17:23:17 +0900

    [ADD] auth 테스트 함수 추가

--- qa/pages/body/signup_page.py
+++ qa/pages/body/signup_page.py
@@ -5,11 +5,12 @@ from pages.base_page import BasePage
 
 class SignUpPage(BasePage):
     PAGE_TITLE = (By.XPATH, '//h1[text()="Sign Up"]')
-    SIGN_IN_LINK = (By.XPATH, '//a[@href="login"]')
+    SIGN_IN_LINK = (By.XPATH, '//a[@href="/login"]')
     USERNAME_INPUT = (By.CSS_SELECTOR, 'input[type="text"]')
     EMAIL_INPUT = (By.CSS_SELECTOR, 'input[type="email"]')
     PASSWORD_INPUT = (By.CSS_SELECTOR, 'input[type="password"]')
     SIGN_UP_BTN = (By.CSS_SELECTOR, 'button[type="submit"]')
+    ERROR_TXT = (By.CSS_SELECTOR, 'ul.error-messages li')
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -23,6 +24,9 @@ class SignUpPage(BasePage):
     def send_password_input(self, password):
         self.send_keys(self.PASSWORD_INPUT, password)
 
+    def click_sign_in_link(self):
+        self.click_element(self.SIGN_IN_LINK)
+
     def click_sign_up_btn(self):
         self.click_element(self.SIGN_UP_BTN)
 
@@ -34,6 +38,13 @@ class SignUpPage(BasePage):
     
     def get_password_input_value(self):
         return self.get_attribute(self.PASSWORD_INPUT, "value")
+    
+    def get_error_message_text(self):
+        return self.find_element(self.ERROR_TXT).text
+    
+    def get_error_messages_text(self):
+        elements = self.find_elements(self.ERROR_TXT)
+        return [element.text.strip() for element in elements]
 
     def sign_up(self):
         self.send_username_input(VALID_USER["username"])
