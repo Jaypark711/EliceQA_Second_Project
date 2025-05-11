f341b574285e0f6b73aae5dfb3516020e1b0d659
Jaypark711 <lossers0@gmail.com>
2025-05-11 12:57:08 +0900

    [ADD] SETTING_01 시나리오 초안 완성

--- qa/pages/body/settings_page.py
+++ qa/pages/body/settings_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from db.execute_query import *
 
 class SettingsPage(BasePage):
     # 로케이터 정의
@@ -9,6 +10,7 @@ class SettingsPage(BasePage):
     BIO_INPUT = (By.CSS_SELECTOR,'textarea[placeholder="Short bio about you"]')
     EMAIL_INPUT = (By.CSS_SELECTOR,'input[placeholder="Email"]')
     PASSWORD_INPUT = (By.CSS_SELECTOR,'input[placeholder="New Password"]')
+    UPDATE_BTN = (By.CSS_SELECTOR, 'button[type="submit"]')
     LOGOUT_BTN = (By.XPATH, '//button[text()="Or click here to logout."]')
 
 
@@ -34,4 +36,24 @@ class SettingsPage(BasePage):
     def click_logout_btn(self):
         self.click_element(self.LOGOUT_BTN)
 
+    def is_value_equal(self, locator, expected):
+        actual = self.get_attribute(locator, "value")
+        return actual == expected
     
+    def is_url_link_equal(self, expected):
+        return self.is_value_equal(self.URL_LINK_INPUT, expected)
+
+    def is_username_equal(self, expected):
+        return self.is_value_equal(self.USERNAME_INPUT, expected)
+
+    def is_bio_equal(self, expected):
+        return self.is_value_equal(self.BIO_INPUT, expected)
+
+    def is_email_equal(self, expected):
+        return self.is_value_equal(self.EMAIL_INPUT, expected)
+    
+    def click_update_btn(self):
+        self.click_element(self.UPDATE_BTN)
+
+    def reset_user_info_by_username(username):
+        update("User", {"username":"user1", "email":"1@1.1"}, {"username":username})
--- qa/tests/test_settings.py
+++ qa/tests/test_settings.py
@@ -1,26 +1,29 @@
 import pytest
-
 from utils.logger import setupLogger
 from pages.header.header import Header
 from pages.body.home_page import HomePage
 from pages.body.signin_page import SignInPage
 from pages.body.settings_page import SettingsPage
+from config.config import SETTINGS_URL
 from db.execute_query import *
 import time
+from config.config import BASE_URL
+from selenium.webdriver.remote.webdriver import WebDriver
 
 @pytest.mark.usefixtures("driver")
 class TestSettings:
     logger = setupLogger(__qualname__)
 
     url = ["https://i.namu.wiki/i/WxfZN24op278LD9Zs0CqcmmDTf5BrsxEj-jKSFSKGshojImAMcxAQET1l6DX5pmCKffUsSm65A1T0yUg0RVILEiobkjl4EHQvprjO7BOl7zzUZv6NjFkt8G1j7O1n98hvxqYx6pUyCLFiSR3hLbwRg.webp","https://item.kakaocdn.net/do/eca065fa2b45d3cc588798b70ee7ec2ff43ad912ad8dd55b04db6a64cddaf76d","Wrong_URL"]
-    username = ["1", "2", "3"]
+    username = ["user1", "user2", "user3"]
     bio = ["Sample bio 1", "Sample bio 2", "Sample bio 3"]
     email = ["1@1.1", "2@2.2", "3@3.3"]
     password = ["1", "2", "3"]
     test_data = list(zip(url, username, bio, email, password))
 
     @pytest.fixture(autouse=True)
-    def teardown(self):
+    def setup_and_teardown(self, driver:WebDriver):
+        driver.get(BASE_URL)
         yield
         self.logger.info("============================")
 
@@ -31,26 +34,41 @@ class TestSettings:
         header = Header(driver)
         loginPage = SignInPage(driver)
         settingsPage = SettingsPage(driver)
+        try:
+            header.click_sign_in_link()
+            loginPage.login()
+            header.click_settings_link()
+            current_url = settingsPage.wait_until_url_and_get(SETTINGS_URL)
 
-        header.click_sign_in_link()
-        loginPage.login()
-        header.click_settings_link()
-        current_url = settingsPage.current_url()
-        settingsPage.send_url_link_input(url)
-        settingsPage.send_username_input(username)
-        settingsPage.send_bio_input(bio)
-        settingsPage.send_email_input(email)
-        
-        assert "/settings" in current_url  , "URL이 일치하지 않음"
-        self.logger.info("Settings 페이지 진입 확인")
-        url_link_input_value = settingsPage.get_attribute(settingsPage.URL_LINK_INPUT, "value")
-        username_input_value = settingsPage.get_attribute(settingsPage.USERNAME_INPUT, "value")
-        bio_input_value = settingsPage.get_attribute(settingsPage.BIO_INPUT, "value")
-        email_input_value = settingsPage.get_attribute(settingsPage.EMAIL_INPUT, "value")
-        assert url == url_link_input_value, "URL 링크 입력값이 일치하지 않음"
-        assert username == username_input_value, "사용자 이름 입력값이 일치하지 않음"
-        assert bio == bio_input_value, "바이오 입력값이 일치하지 않음"
-        assert email == email_input_value, "이메일 입력값이 일치하지 않음"
+            assert SETTINGS_URL in current_url # 기대 결과 1
+            self.logger.info("기대 결과 1 - Settings 페이지로 진입됨")
 
+            settingsPage.send_url_link_input(url)
+            settingsPage.send_username_input(username)
+            settingsPage.send_bio_input(bio)
+            settingsPage.send_email_input(email)
 
-        self.logger.info("회원 정보 입력 및 제출 완료")
+            assert all([ # 기대결과 2
+            settingsPage.is_url_link_equal(url),
+            settingsPage.is_username_equal(username),
+            settingsPage.is_bio_equal(bio),
+            settingsPage.is_email_equal(email)
+            ])
+            self.logger.info("기대 결과 2 - 입력한 값이 입력 필드에 입력됨")
+
+            settingsPage.click_update_btn()
+            print(f"header url = {header.get_profile_img()}")
+            print(f"header username = {header.get_username()}")
+            print(f"url = {url}")
+            print(f"username = {username}")
+            assert all([
+                url in header.get_profile_img(),
+                header.get_username() == username
+            ])
+            self.logger.info("기대 결과 3 - 프로필 사진과 계정명이 헤더에 노출됨")
+            self.logger.info("회원 정보 입력 및 제출 완료")
+        except Exception as e:
+            self.logger.error(f"❌ 회원 정보 업데이트 테스트 중 오류 발생: {e}")
+            assert False, "❌ 회원 정보 업데이트 테스트 중 오류 발생"
+        finally:
+            update("User", {"username":"user1", "email":"1@1.1"}, {"username":username}) # 테스트 종료 후 username, email 정보 초기화
