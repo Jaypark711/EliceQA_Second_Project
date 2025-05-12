6badd1efa03a53c245c906a5c0952007bd9284f3
kys0529 <rkddustn2519@naver.com>
2025-05-12 15:02:33 +0900

    [ADD] test_filter 테스트 함수 추가

--- qa/tests/test_filter.py
+++ qa/tests/test_filter.py
@@ -4,6 +4,7 @@ from config.config import BASE_URL
 from pages.header.header import Header
 from pages.body.signup_page import SignUpPage
 from pages.body.home_page import HomePage
+from pages.body.article_page import ArticlePage
 from utils.logger import setupLogger
 from db.db_utils import delete_all_user, run_prisma_seed, get_article_count_by_tag_name
 
@@ -21,31 +22,37 @@ class TestFilter():
 
         self.logger.info("==================================")
 
-    # def test_filter_articles_by_tag(self, driver):
-    #     """FILTER_01: 태그 선택 시 해당 태그를 포함한 게시글만 필터링되어 표시되는지 확인"""
-    #     self.logger.info("태그 선택 및 게시글 필터링 테스트 시작")
-    #     header = Header(driver)
-    #     home = HomePage(driver)
-    #     signUpPage = SignUpPage(driver)
+    def test_filter_articles_by_tag(self, driver):
+        """FILTER_01: 태그 선택 시 해당 태그를 포함한 게시글만 필터링되어 표시되는지 확인"""
+        self.logger.info("태그 선택 및 게시글 필터링 테스트 시작")
+        header = Header(driver)
+        home = HomePage(driver)
+        signUpPage = SignUpPage(driver)
+        articlePage = ArticlePage(driver)
 
-    #     try:
-    #         header.click_sign_up_link()
-    #         signUpPage.sign_up()
+        try:
+            # 테스트 환경 세팅
+            header.click_sign_up_link()
+            signUpPage.sign_up()
 
-    #         clicked_tag_text = home.click_random_popular_tag_link()
-    #         self.logger.info("인기 태그 목록에서 무작위 태그 클릭 및 해당 태그 텍스트 추출 완료")
+            # 테스트 시나리오 시작
+            clicked_tag_text = home.click_random_popular_tag_link()
+            self.logger.info("인기 태그 목록에서 무작위 태그 클릭 및 해당 태그 텍스트 추출 완료")
 
-    #         tag_tab_text = home.get_tag_tab_link_text()
-    #         self.logger.info("화면에 표시된 태그 탭 텍스트 추출 완료")
+            tag_tab_text = home.get_tag_tab_link_text()
+            self.logger.info("화면에 표시된 태그 탭 텍스트 추출 완료")
 
-    #         assert clicked_tag_text == tag_tab_text
-    #         self.logger.info("클릭한 인기 태그와 화면에 표시된 태그 탭 텍스트가 일치함을 확인")
+            assert clicked_tag_text == tag_tab_text # 기대 결과 1: 선택된 태그와 이름이 동일한 탭이 Global Feed 우측에 추가되어야 함
+            self.logger.info("클릭한 인기 태그와 화면에 표시된 태그 탭 텍스트가 일치함을 확인")
 
-    #         # TODO: 태그를 포함한 게시글 필터링 확인 (게시글 정보 불러오는 함수 구현 전까지 대기)
+            articles = articlePage.load_article_preview_lists()
+            for article_id, article_data in articles.items():
+                assert clicked_tag_text in article_data["tags"] # 기대 결과 2: 태그가 포함된 게시글만 필터링되어 표시되어야 함
+            self.logger.info("클릭한 태그를 포함한 게시글만 필터링되어 표시됨을 확인")
 
-    #     except Exception as e:
-    #         self.logger.error(f"❌ FILTER_01 테스트 중 오류 발생: {e}")
-    #         assert False
+        except Exception as e:
+            self.logger.error(f"❌ FILTER_01 테스트 중 오류 발생: {e}")
+            assert False
 
     def test_popular_tags_update_correctly(self, driver):
         """FILTER_02: Popular Tags가 게시글 태그 등록 상황에 따라 정확히 반영되는지 확인"""
