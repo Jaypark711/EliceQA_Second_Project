5577a8ec2cea4d44f4cf9024b1b4fca12fc9d09e
YANA <ysi1246@daum.net>
2025-05-12 14:38:42 +0900

    [ADD] utils > helpers.py
    - base_page.py에 있던 wait_until_page_load_complete()를 helpers.py로 이동

---
+++ qa/utils/helpers.py
@@ -0,0 +1,14 @@
+from config.config import TIMEOUT
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class Helpers:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, TIMEOUT)
+
+    # 페이지 로드가 완료될 때 까지 대기
+    def wait_until_page_load_complete(self):
+        self.wait.until(
+            lambda d: d.execute_script("return document.readyState") == "complete"
+        )
