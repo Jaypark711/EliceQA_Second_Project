219e64f58d8e96c294598f88a9710e7910e1702c
Jaypark711 <lossers0@gmail.com>
2025-05-13 18:59:10 +0900

    [REFACTORING] test_auth 완성

--- qa/tests/test_auth.py
+++ qa/tests/test_auth.py
@@ -24,42 +24,46 @@ class TestAuthentication:
 
         yield
 
-        self.logger.info("==================================")
+        self.logger.info("=================================================================")
 
     @allure.title("[AUTH_01]: 인증 - 회원가입")
     @allure.description("유효한 정보로 회원가입 성공 및 사용자 정보 확인")
     @allure.severity(allure.severity_level.CRITICAL)
     def test_successful_signup(self, driver):
-        self.logger.info("회원가입 테스트 시작")
+        self.logger.info("▶️ 회원가입 테스트 시작")
         header = Header(driver)
         homePage = HomePage(driver)
         signUpPage = SignUpPage(driver)
 
         try:
             header.click_sign_up_link()
-            assert header.is_go_to_sign_up_page() # 기대 결과 1: Sign up 페이지로 진입되어야 함
+            assert header.is_go_to_sign_up_page()
+            self.logger.info("✅ 기대 결과 1: Sign up 페이지로 진입 됨")
 
             signUpPage.send_username_input(VALID_USER["username"])
-            assert signUpPage.get_username_input_value() == VALID_USER["username"] # 기대 결과 2: {사용자명}이 "Username" 입력 필드에 정상 반영됨
+            assert signUpPage.get_username_input_value() == VALID_USER["username"]
+            self.logger.info("✅ 기대 결과 2: 사용자명이 Username 입력 필드에 정상 반영됨")
 
             signUpPage.send_email_input(VALID_USER["email"])
-            assert signUpPage.get_email_input_value() == VALID_USER["email"] # 기대 결과 3: {이메일}이 "Email" 입력 필드에 정상 반영됨
+            assert signUpPage.get_email_input_value() == VALID_USER["email"]
+            self.logger.info("✅ 기대 결과 3: 이메일이 Email 입력 필드에 정상 반영됨")
 
             signUpPage.send_password_input(VALID_USER["password"])
-            assert signUpPage.get_password_input_value() == VALID_USER["password"] # 기대 결과 4: {비밀번호}가 "Password" 입력 필드에 정상 반영됨
-        
-            signUpPage.click_sign_up_btn()
-            assert header.is_go_to_home_page() # 기대 결과 5: 메인 화면 진입  
-            self.logger.info("회원가입 정보 입력 및 제출 완료")
+            assert signUpPage.get_password_input_value() == VALID_USER["password"]
+            self.logger.info("✅ 기대 결과 4: 비밀번호가 Password 입력 필드에 정상 반영됨")
 
-            assert all([ # 기대 결과 6: 메인 화면에 Your Feed 탭, New Post 링크, Settings 링크, MyProfile 링크 추가 표시
+            signUpPage.click_sign_up_btn()
+            assert header.is_go_to_home_page()
+            self.logger.info("✅ 기대 결과 5: 메인 화면 진입")
+
+            assert all([
                 homePage.is_your_feed_tab_link_appear(),
                 header.is_new_post_link_appear(),
                 header.is_settings_link_appear(),
                 header.is_my_profile_link_appear()
             ])
-            self.logger.info("사용자 정보 확인 완료")
-            self.logger.info("유효한 정보로 회원가입 테스트 성공")
+            self.logger.info("✅ 기대 결과 6: 사용자 정보 확인 완료")
+            self.logger.info("🎉 유효한 정보로 회원가입 테스트 성공")
         except Exception as e:
             self.logger.error(f"❌ AUTH_01 테스트 중 오류 발생: {e}")
             assert False, "❌ AUTH_01 테스트 중 오류 발생"
@@ -74,7 +78,7 @@ class TestAuthentication:
         (VALID_USER["username"], VALID_USER["email"], VALID_USER["password"], ["email has already been taken", "username has already been taken"], "유효한 입력값")
     ])
     def test_fail_signup(self, driver, username, email, password, expected_result, description):
-        self.logger.info(f"{description}(으)로 회원가입 테스트 시작")
+        self.logger.info(f"▶️ {description}(으)로 회원가입 테스트 시작")
         header = Header(driver)
         signInPage = SignInPage(driver)
         signUpPage = SignUpPage(driver)
@@ -89,23 +93,28 @@ class TestAuthentication:
 
             # 테스트 시나리오 시작
             header.click_sign_up_link()
-            assert header.is_go_to_sign_up_page() # 기대 결과 1: Sign up 페이지로 진입되어야 함
+            assert header.is_go_to_sign_up_page()
+            self.logger.info("✅ 기대 결과 1: Sign up 페이지로 진입됨")
 
             signUpPage.send_username_input(username)
-            assert signUpPage.get_username_input_value() == username # 기대 결과 2: {사용자명}이 "Username" 입력 필드에 정상 반영됨
+            assert signUpPage.get_username_input_value() == username
+            self.logger.info("✅ 기대 결과 2: 사용자명이 Username 입력 필드에 정상 반영됨")
 
             signUpPage.send_email_input(email)
-            assert signUpPage.get_email_input_value() == email # 기대 결과 3: {이메일}이 "Email" 입력 필드에 정상 반영됨
+            assert signUpPage.get_email_input_value() == email
+            self.logger.info("✅ 기대 결과 3: 이메일이 Email 입력 필드에 정상 반영됨")
 
             signUpPage.send_password_input(password)
-            assert signUpPage.get_password_input_value() == password # 기대 결과 4: {비밀번호}가 "Password" 입력 필드에 정상 반영됨
+            assert signUpPage.get_password_input_value() == password
+            self.logger.info("✅ 기대 결과 4: 비밀번호가 Password 입력 필드에 정상 반영됨") 
 
             signUpPage.click_sign_up_btn()
             if isinstance(expected_result, str):
-                assert expected_result in signUpPage.get_error_message_text() # 기대 결과 5: {경고창}이 표시됨
+                assert expected_result in signUpPage.get_error_message_text()
             else:
-                assert set(signUpPage.get_error_messages_text()) == set(expected_result) # 기대 결과 5: {경고창}이 표시됨
-            self.logger.info(f"{description}(으)로 회원가입 테스트 시 {expected_result} 경고창 출력 확인")
+                assert set(signUpPage.get_error_messages_text()) == set(expected_result)
+            self.logger.info(f"✅ 기대 결과 5: {description}(으)로 회원가입 테스트 시 {expected_result} 경고창 출력 확인")
+            self.logger.info(f"🎉 {description}(으)로 회원가입 테스트 완료")
 
         except Exception as e:
             self.logger.error(f"❌ AUTH_02 테스트 중 오류 발생: {e}")
@@ -115,7 +124,7 @@ class TestAuthentication:
     @allure.description("유효한 정보로 로그인 성공 및 사용자 정보 확인")
     @allure.severity(allure.severity_level.NORMAL)
     def test_successful_signin(self, driver):
-        self.logger.info("유효한 정보로 로그인 테스트 시작")
+        self.logger.info("▶️ 유효한 정보로 로그인 테스트 시작")
         header = Header(driver)
         homePage = HomePage(driver)
         signInPage = SignInPage(driver)
@@ -134,14 +143,16 @@ class TestAuthentication:
             assert header.is_go_to_sign_in_page() # 기대 결과 1: Sign in 페이지로 진입되어야 함
 
             signInPage.send_email_input(VALID_USER["email"])
-            assert signInPage.get_email_input_value() == VALID_USER["email"] # 기대 결과 2: {이메일}이 "Email" 입력 필드에 정상 반영됨
+            assert signInPage.get_email_input_value() == VALID_USER["email"]
+            self.logger.info("✅ 기대 결과 2: 이메일이 Email 입력 필드에 정상 반영됨")
 
             signInPage.send_password_input(VALID_USER["password"])
-            assert signInPage.get_password_input_value() == VALID_USER["password"] # 기대 결과 3: {비밀번호}가 "Password" 입력 필드에 정상 반영됨
+            assert signInPage.get_password_input_value() == VALID_USER["password"]
+            self.logger.info("✅ 기대 결과 3: 비밀번호가 Password 입력 필드에 정상 반영됨")
 
             signInPage.click_sign_in_btn()
-            assert header.is_go_to_home_page() # 기대 결과 4: 메인 화면 진입  
-            self.logger.info("로그인 정보 입력 및 제출 완료")
+            assert header.is_go_to_home_page()
+            self.logger.info("✅ 기대 결과 4: 메인 화면 진입")
 
             assert all([ # 기대 결과 5: 메인 화면에 Your Feed 탭, New Post 링크, Settings 링크, MyProfile 링크 추가 표시
                 homePage.is_your_feed_tab_link_appear(),
@@ -149,8 +160,8 @@ class TestAuthentication:
                 header.is_settings_link_appear(),
                 header.is_my_profile_link_appear()
             ])
-            self.logger.info("사용자 정보 확인 완료")
-            self.logger.info("유효한 정보로 로그인 테스트 성공")
+            self.logger.info("✅ 기대 결과 5: 메인 화면에 Your Feed 탭, New Post 링크, Settings 링크, MyProfile 링크 추가 표시")
+            self.logger.info("🎉 유효한 정보로 로그인 테스트 성공")
 
         except Exception as e:
             self.logger.error(f"❌ AUTH_03 테스트 중 오류 발생: {e}")
@@ -207,7 +218,7 @@ class TestAuthentication:
         ("user@", "'@' 뒷 부분을 입력해 주세요. 'user@'(이)가 완전하지 않습니다.")
     ])
     def test_email_validation_message(self, driver, email, expected_result):
-        self.logger.info("이메일 유효성 검사 메시지 테스트 시작")
+        self.logger.info("▶️ 이메일 유효성 검사 메시지 테스트 시작")
         header = Header(driver)
         signInPage = SignInPage(driver)
         signUpPage = SignUpPage(driver)
@@ -220,7 +231,7 @@ class TestAuthentication:
 
             signInPage.click_sign_in_btn()
             assert signInPage.get_email_validation_message() == expected_result # 기대 결과 2: {경고창}이 표시됨
-            self.logger.info("로그인 페이지에서 이메일 유효성 검사 메시지 확인")
+            self.logger.info("✅ 로그인 페이지에서 이메일 유효성 검사 메시지 확인")
 
             header.click_sign_up_link()
             signUpPage.send_email_input(email)
@@ -228,8 +239,8 @@ class TestAuthentication:
 
             signUpPage.click_sign_up_btn()
             assert signUpPage.get_email_validation_message() == expected_result # 기대 결과 4: {경고창}이 표시됨
-            self.logger.info("회원가입 페이지에서 이메일 유효성 검사 메시지 확인")
-            self.logger.info(f"{email}로 이메일 입력 시 {expected_result} 유효성 검사 메시지 확인 완료")
+            self.logger.info("✅ 회원가입 페이지에서 이메일 유효성 검사 메시지 확인")
+            self.logger.info(f"🎉 {email}로 이메일 입력 시 {expected_result} 유효성 검사 메시지 확인 완료")
 
         except Exception as e:
             self.logger.error(f"❌ AUTH_05 테스트 중 오류 발생: {e}")
@@ -239,7 +250,7 @@ class TestAuthentication:
     @allure.description("회원가입 페이지와 로그인 페이지 간의 리다이렉션 기능 확인")
     @allure.severity(allure.severity_level.NORMAL)
     def test_redirection(self, driver):
-        self.logger.info("리다이렉션 테스트 시작")
+        self.logger.info("▶️ 리다이렉션 테스트 시작")
         header = Header(driver)
         signInPage = SignInPage(driver)
         signUpPage = SignUpPage(driver)
@@ -247,15 +258,17 @@ class TestAuthentication:
         try:
             # 테스트 시나리오 시작
             header.click_sign_in_link()
-            assert header.is_go_to_sign_in_page() # 기대 결과 1: Sign in 페이지로 진입되어야 함
+            assert header.is_go_to_sign_in_page()
+            self.logger.info("✅ 기대 결과 1: Sign in 페이지로 진입됨")
             
             signInPage.click_sign_up_link()
-            assert header.is_go_to_sign_up_page() # 기대 결과 2: Sign up 페이지로 진입되어야 함
+            assert header.is_go_to_sign_up_page()
+            self.logger.info("✅ 기대 결과 2: Sign up 페이지로 진입됨") 
 
             signUpPage.click_sign_in_link()
-            assert header.is_go_to_sign_in_page() # 기대 결과 3: Sign in 페이지로 진입되어야 함
-            self.logger.info("리다이렉션 테스트 성공")
-
+            assert header.is_go_to_sign_in_page()
+            self.logger.info("✅ 기대 결과 3: Sign in 페이지로 진입됨")
+            self.logger.info("🎉 리다이렉션 테스트 완료")
         except Exception as e:
             self.logger.error(f"❌ AUTH_06 테스트 중 오류 발생: {e}")
             assert False, "❌ AUTH_06 테스트 중 오류 발생"
@@ -263,7 +276,7 @@ class TestAuthentication:
     @allure.title("[AUTH_07]: 인증 - 로그아웃")
     @allure.description("로그인 상태에서 로그아웃 성공")
     def test_logout(self, driver):
-        self.logger.info("로그아웃 테스트 시작")
+        self.logger.info("▶️ 로그아웃 테스트 시작")
         header = Header(driver)
         homePage = HomePage(driver)
         signUpPage = SignUpPage(driver)
@@ -276,18 +289,21 @@ class TestAuthentication:
 
             # 테스트 시나리오 시작
             header.click_settings_link()
-            assert header.is_go_to_settings_page() # 기대 결과 1: Settings 페이지로 진입되어야 함
+            assert header.is_go_to_settings_page()
+            self.logger.info("✅ 기대 결과 1: Settings 페이지로 진입됨")
 
             settingsPage.click_logout_btn()
-            assert header.is_go_to_home_page() # 기대 결과 2: 메인 화면 진입
+            assert header.is_go_to_home_page() # 
+            self.logger.info("✅ 기대 결과 2: 메인 화면 진입")
 
-            assert all([ # 기대 결과 3: 메인 화면에서 Your Feed 탭, New Post 링크, Settings 링크, MyProfile 링크 표시되지 않음
+            assert all([ 
                 homePage.is_your_feed_tab_link_disappear(),
                 header.is_new_post_link_disappear(),
                 header.is_settings_link_disappear(),
                 header.is_my_profile_link_disappear()
             ])
-            self.logger.info("로그아웃 테스트 성공")
+            self.logger.info("✅ 기대 결과 3: 메인 화면에서 Your Feed 탭, New Post 링크, Settings 링크, MyProfile 링크 표시되지 않음")
+            self.logger.info("🎉 로그아웃 테스트 성공")
 
         except Exception as e:
             self.logger.error(f"❌ AUTH_07 테스트 중 오류 발생: {e}")
