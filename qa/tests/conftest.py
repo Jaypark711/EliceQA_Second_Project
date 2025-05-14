e8a5419c655fb581dd6d044bf9c5769a43a00f17
[QA1] 강연수 <rkddustn2519@naver.com>
2025-05-14 11:43:37 +0000

    Update conftest.py

--- qa/tests/conftest.py
+++ qa/tests/conftest.py
@@ -26,6 +26,7 @@ def driver():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
     options.add_experimental_option("excludeSwitches", ['enable-logging'])
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
