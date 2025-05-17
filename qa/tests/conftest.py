69dd33a240ffe5e0c044857868f026e20aa6e58f
Jaypark711 <lossers0@gmail.com>
2025-05-17 14:31:34 +0900

    [ADD] 한글버전 브라우저로 변경 시도

--- qa/tests/conftest.py
+++ qa/tests/conftest.py
@@ -37,10 +37,12 @@ def pytest_sessionfinish(session, exitstatus):
 @pytest.fixture(scope='function')
 def driver():
     options = Options()
+    options.add_argument("--lang=ko")
     options.add_argument('--start-maximized')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
+    options.add_argument("--headless=new")
     options.add_experimental_option("excludeSwitches", ['enable-logging'])
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
